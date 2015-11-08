0a9b4acbff5b44039f2166bb047961459bf84a30
Carlos <512.quadraphonic@gmail.com>
2015-11-08 17:48:02 -0500

    initial commit with a library to talk to the lidar sensor

---
+++ lidar/lidar.h
@@ -0,0 +1,33 @@
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include "i2c-dev.h"
+
+#define DEF_ADDR 0x62
+
+//register addresses
+#define CONTROL_REG 0x00
+#define STATUS_REG 0x01
+#define AQUISIT_REG 0x8f
+#define SERIAL_REG 0x96
+
+// commands
+#define RESET_FPGA 0x00
+#define AQUISIT_DC 0x04
+#define AQUISIT_NODC 0x03
+
+// status register bits
+#define MEASURE_ERROR 0x40
+#define DEVICE_OKAY 0x20
+#define INVALID_SIGNAL 0x08
+#define DEVICE_READY 0x01
+
+int openDevice(char* filename);
+
+int getDistance(int file);
+
+int isOkay(int file);
+
+int getDeviceStatus(int file);
+
+int isReady(int file);
