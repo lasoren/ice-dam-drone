67c11357e81725796a2851efd2d8740eee476ce6
Luke Sorenson <lasoren@me.com>
2015-11-09 13:23:39 -0500

    Creating more positive samples from the 30 images of icicles

---
+++ image-processing/rgb/experimental/cascading-classifier/run_createsamples.py
@@ -0,0 +1,4 @@
+import os
+
+for i in range(25, 31):
+    os.system("opencv_createsamples -img positive_images/" + str(i) + ".jpg -bg bg.txt -info positive_images/" + str(i) + "/info.dat -maxxangle 0.1 -maxyangle 0.1 -maxzangle 0.1 -bgcolor 0 -bgthresh 0")
