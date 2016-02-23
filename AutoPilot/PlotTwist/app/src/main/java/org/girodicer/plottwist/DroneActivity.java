45b85ac184cceff2d4d0d1e7c9a1307685103dca
Carlos Cheung <carlos@tumvi.com>
2016-02-23 15:21:09 -0500

    Layout creation

--- AutoPilot/PlotTwist/app/src/main/java/org/girodicer/plottwist/DroneActivity.java
+++ AutoPilot/PlotTwist/app/src/main/java/org/girodicer/plottwist/DroneActivity.java
@@ -1,5 +1,14 @@
 package org.girodicer.plottwist;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,6 +27,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.girodicer.plottwist.Bluetooth.BluetoothException;
+import org.girodicer.plottwist.Bluetooth.ConnectionThread;
+import org.girodicer.plottwist.Bluetooth.GProtocol;
+import org.girodicer.plottwist.Models.Status;
+import org.girodicer.plottwist.services.BluetoothService;
+
+import java.util.ArrayList;
 
 public class DroneActivity extends AppCompatActivity {
 
@@ -36,9 +56,45 @@ public class DroneActivity extends AppCompatActivity {
      */
     private ViewPager mViewPager;
 
+    private Status currentStatus;
+
+    private final Messenger btMessageHandler = new Messenger(new BTMessageHandler());
+
+    private static Messenger bluetoothMessenger; // only for the handler in this class
+    private static boolean bluetoothServiceBound = false;
+
+    private ServiceConnection bluetoothConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            bluetoothMessenger = new Messenger(service);
+            bluetoothServiceBound = true;
+
+            Message msg = Message.obtain(null, BluetoothService.MESSAGE_NEW_CLIENT);
+            msg.replyTo = btMessageHandler;
+            try {
+                bluetoothMessenger.send(msg);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Message msg = Message.obtain(null, BluetoothService.MESSAGE_DETACH_CLIENT);
+            try{
+                bluetoothMessenger.send(msg);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+            bluetoothMessenger = null;
+            bluetoothServiceBound = false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_drone);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -87,6 +143,18 @@ public class DroneActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        bindService(new Intent(this, BluetoothService.class), bluetoothConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unbindService(bluetoothConnection);
     }
 
     /**
@@ -158,6 +226,39 @@ public class DroneActivity extends AppCompatActivity {
                     return "SECTION 3";
             }
             return null;
+        }
+    }
+
+    private class BTMessageHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg){
+            switch(msg.what){
+                case BluetoothService.MESSAGE_READ:
+                    Bundle bundle = msg.getData();
+                    byte[] data = bundle.getByteArray(ConnectionThread.BT_DATA);
+                    try {
+                        GProtocol received = GProtocol.Unpack(data);
+                        switch(received.getCommand()){
+                            case GProtocol.COMMAND_STATUS:
+                                currentStatus = (Status) received.read();
+                                break;
+                            case GProtocol.COMMAND_SEND_POINTS:
+                                break;
+                        }
+                    } catch (BluetoothException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case BluetoothService.MESSAGE_BT_CONNECTION_LOST:
+                    Toast.makeText(DroneActivity.this, "Connection lost", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(DroneActivity.this, "Reconnecting....", Toast.LENGTH_LONG).show();
+                    break;
+                case BluetoothService.MESSAGE_BT_FAILED_RECONNECT:
+                    Toast.makeText(DroneActivity.this, "Still reconnecting", Toast.LENGTH_SHORT).show();
+                    break;
+                case BluetoothService.MESSAGE_BT_SUCCESS_RECONNECT:
+                    Toast.makeText(DroneActivity.this, "Successfully reconnected", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
---
+++ AutoPilot/PlotTwist/app/src/main/java/org/girodicer/plottwist/Fragments/DroneState.java
@@ -0,0 +1,15 @@
+package org.girodicer.plottwist.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Carlos on 2/23/2016.
+ */
+public class DroneState extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+    }
+}
