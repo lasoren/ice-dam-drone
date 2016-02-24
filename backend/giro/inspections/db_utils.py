99c9c20365c0adb5956b6376322202bbd84f8244
Luke Sorenson <lasoren@me.com>
2016-02-24 00:34:34 -0500

    Updated gitignore

--- backend/giro/inspections/db_utils.py
+++ backend/giro/inspections/db_utils.py
@@ -7,7 +7,7 @@ def add_to_inspection_provision(inspection_id):
     Adds a row to the client provision, and deletes old rows
     """
     InspectionProvision.objects.filter(
-        inspection_id=client_id).delete()
+        inspection_id=inspection_id).delete()
     provision = InspectionProvision.objects.create(
-        inspection_id=client_id)
+        inspection_id=inspection_id)
     provision.save()
