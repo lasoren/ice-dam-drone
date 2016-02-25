d71ede4efea8ede1e0a8f777f1640fb5f3643a7a
Berroa <tberroa@bu.edu>
2016-02-25 16:36:28 -0500

    minor ui tweek: added screen elements to past inspections activity when there are no inspections to display

--- android/GirodicerApp/app/src/main/java/com/example/tberroa/girodicerapp/activities/PastInspectionsActivity.java
+++ android/GirodicerApp/app/src/main/java/com/example/tberroa/girodicerapp/activities/PastInspectionsActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.tberroa.girodicerapp.data.ClientId;
 import com.example.tberroa.girodicerapp.database.LocalDB;
@@ -15,6 +17,8 @@ import com.example.tberroa.girodicerapp.R;
 import com.example.tberroa.girodicerapp.helpers.Utilities;
 import com.example.tberroa.girodicerapp.models.Inspection;
 import com.example.tberroa.girodicerapp.models.InspectionImage;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -44,18 +48,34 @@ public class PastInspectionsActivity extends BaseActivity {
         });
         toolbar.setVisibility(View.VISIBLE);
 
+        // check to see if their are any inspections
+        if (inspections.size() != 0){
+            // initialize recycler view
+            int span = Utilities.getSpanGrid(this);
+            int spacing = Utilities.getSpacingGrid(this);
+            final RecyclerView recyclerView = (RecyclerView)findViewById(R.id.past_inspections_recycler_view);
+            recyclerView.setLayoutManager(new GridLayoutManager(this, span));
+            recyclerView.addItemDecoration(new GridSpacingItemDecoration(span, spacing));
+            recyclerView.setVisibility(View.VISIBLE);
 
-        // initialize recycler view
-        int span = Utilities.getSpanGrid(this);
-        int spacing = Utilities.getSpacingGrid(this);
-        final RecyclerView recyclerView = (RecyclerView)findViewById(R.id.past_inspections_recycler_view);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, span));
-        recyclerView.addItemDecoration(new GridSpacingItemDecoration(span, spacing));
-
-        // populate view with past inspections
-        PastInspectionsViewAdapter pastInspectionsViewAdapter;
-        pastInspectionsViewAdapter = new PastInspectionsViewAdapter(this, inspections);
-        recyclerView.setAdapter(pastInspectionsViewAdapter);
+            // populate view with past inspections
+            PastInspectionsViewAdapter pastInspectionsViewAdapter;
+            pastInspectionsViewAdapter = new PastInspectionsViewAdapter(this, inspections);
+            recyclerView.setAdapter(pastInspectionsViewAdapter);
+        }
+        else{ // no inspections
+            // initialize text view and button
+            TextView noInspectionsText = (TextView) findViewById(R.id.no_past_inspections_text);
+            noInspectionsText.setVisibility(View.VISIBLE);
+            Button startInspectionButton = (Button) findViewById(R.id.start_inspection);
+            startInspectionButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Utilities.AttemptInspectionStart(PastInspectionsActivity.this);
+                }
+            });
+            startInspectionButton.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
