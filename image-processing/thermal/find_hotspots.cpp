fe2bbb4f8a490de7240770c421d8eca06b0d0b22
Luke Sorenson <lasoren@me.com>
2016-03-24 18:33:20 -0400

    Computing mean and standard deviation of image in order to perform thresholding

--- image-processing/thermal/find_hotspots.cpp
+++ image-processing/thermal/find_hotspots.cpp
@@ -9,6 +9,9 @@ using namespace cv;
 using namespace std;
 
 int LoadImage(const char* filename, Mat* original);
+void FindHotspots(const Mat& input, Mat* output);
+
+const char kDetectWindowName[] = "Source with Hotspots Found";
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -19,8 +22,22 @@ int main(int argc, char* argv[]) {
     if (LoadImage(argv[1], &detect) == -1) {
         return -1;
     }
+    Mat output;
+    FindHotspots(detect, &output);
+    namedWindow(kDetectWindowName, WINDOW_AUTOSIZE);
+//    imshow(kDetectWindowName, output);
     // Wait for user to hit any key to end the program.
     waitKey(0);
+}
+
+void FindHotspots(const Mat& input, Mat* output) {
+    Mat grayscale;
+    // Convert the input image to grayscale.
+    cvtColor(input, grayscale, CV_BGR2GRAY); 
+    // Compute the mean value of the image and the standard deviation.
+    Scalar mean, std_dev;
+    meanStdDev(grayscale, mean, std_dev); 
+    cout << mean << "   " << std_dev << endl;
 }
 
 int LoadImage(const char* filename, Mat* original) {
