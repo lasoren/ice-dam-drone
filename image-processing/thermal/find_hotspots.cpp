7ee1eca9fc3c13c49ff9c1b2f007b1cf36004a6b
Luke Sorenson <lasoren@me.com>
2016-03-25 21:04:11 -0400

    DONE - hotspot detection works

--- image-processing/thermal/find_hotspots.cpp
+++ image-processing/thermal/find_hotspots.cpp
@@ -1,4 +1,3 @@
-
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
@@ -9,7 +8,7 @@ using namespace cv;
 using namespace std;
 
 int LoadImage(const char* filename, Mat* original);
-void FindHotspots(const Mat& input, Mat* output);
+void FindHotspots(Mat& input, Mat& output);
 
 const char kDetectWindowName[] = "Source with Hotspots Found";
 
@@ -23,21 +22,31 @@ int main(int argc, char* argv[]) {
         return -1;
     }
     Mat output;
-    FindHotspots(detect, &output);
+    FindHotspots(detect, output);
     namedWindow(kDetectWindowName, WINDOW_AUTOSIZE);
-//    imshow(kDetectWindowName, output);
     // Wait for user to hit any key to end the program.
+    imshow(kDetectWindowName, detect);
     waitKey(0);
 }
 
-void FindHotspots(const Mat& input, Mat* output) {
-    Mat grayscale;
+void FindHotspots(Mat& input, Mat& output) {
     // Convert the input image to grayscale.
-    cvtColor(input, grayscale, CV_BGR2GRAY); 
+    cvtColor(input, output, CV_BGR2GRAY); 
     // Compute the mean value of the image and the standard deviation.
     Scalar mean, std_dev;
-    meanStdDev(grayscale, mean, std_dev); 
-    cout << mean << "   " << std_dev << endl;
+    meanStdDev(output, mean, std_dev);
+    // Threshold the image on the mean plus the standard deviation.
+    threshold(output, output, mean[0] + std_dev[0], 255, THRESH_BINARY);
+    // Blur the image in order to detect blobs. 
+    GaussianBlur(output, output, Size(7, 7), 0, 0 );
+    // Detect keypoints.
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    findContours(output, contours, hierarchy,
+            CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+    cvtColor(output, output, CV_GRAY2BGR); 
+    drawContours(output, contours, -1, Scalar(0, 0, 255), CV_FILLED);
+    addWeighted(input, 1.0, output, 0.4, 0.0, input);
 }
 
 int LoadImage(const char* filename, Mat* original) {
