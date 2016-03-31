8cf18f54097326448498e0fceab3c892e1fdbe6d
Luke Sorenson <lasoren@me.com>
2016-03-30 21:06:26 -0400

    Removing un-needed prints

--- AutoPilot/salt-placement/kmeans_icicle_clusterer.py
+++ AutoPilot/salt-placement/kmeans_icicle_clusterer.py
@@ -8,7 +8,6 @@ class KMeansIcicleClusterer(object):
         self.x_pos = x_pos
         for pos in x_pos:
             self.bracketed.append([pos])
-        print self.bracketed
 
     def cluster(self):
         num_pos = len(self.x_pos)
@@ -25,7 +24,6 @@ class KMeansIcicleClusterer(object):
         centroids = []
         for i in xrange(k):
             centroids.append([min_pos+div*(i*2+1)])
-        print centroids
         # Run the clustering algorithm.
         kme = kmeans_euclid(self.bracketed, centroids, 0.1)
         # Return the centroids.
