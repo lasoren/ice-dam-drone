288aae4042ab432d23ad3a28b796a1afaf8847da
Luke Sorenson <lasoren@me.com>
2016-03-31 15:08:23 -0400

    Added .jpg image extension

--- backend/giro/inspections/utils.py
+++ backend/giro/inspections/utils.py
@@ -4,4 +4,4 @@ import giro.short_url as short_url
 def generate_image_path(inspection_id, inspection_image_id):
     inspection_code = short_url.encode_url(inspection_id)
     image_code = short_url.encode_url(inspection_image_id)
-    return inspection_code + '/' + image_code
+    return inspection_code + '/' + image_code + ".jpg"
