d6da5a4cd1e8221cf7c9e2aeed8d4f18bee57698
Berroa <tberroa@bu.edu>
2016-04-06 16:05:00 -0400

    update

--- android/GirodicerApp/app/src/main/java/com/example/tberroa/girodicerapp/services/SignInIntentService.java
+++ android/GirodicerApp/app/src/main/java/com/example/tberroa/girodicerapp/services/SignInIntentService.java
@@ -2,8 +2,8 @@ package com.example.tberroa.girodicerapp.services;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.util.Log;
 
+import com.activeandroid.ActiveAndroid;
 import com.example.tberroa.girodicerapp.data.Params;
 import com.example.tberroa.girodicerapp.data.UserInfo;
 import com.example.tberroa.girodicerapp.database.LocalDB;
@@ -11,13 +11,7 @@ import com.example.tberroa.girodicerapp.database.ServerDB;
 import com.example.tberroa.girodicerapp.models.Client;
 import com.example.tberroa.girodicerapp.models.Inspection;
 import com.example.tberroa.girodicerapp.models.InspectionImage;
-import com.example.tberroa.girodicerapp.network.Http;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 
-import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.Iterator;
 import java.util.List;
 
@@ -34,71 +28,71 @@ public class SignInIntentService extends IntentService {
         // get clients from server & save them locally
         List<Client> clients = serverDB.getClients();
         if (clients != null && !clients.isEmpty()) {
-            for (int i = 0; i < clients.size(); i++) {
-                Client client = clients.get(clients.size() - 1 - i);
-                if (client.user != null) {
-                    client.cascadeSave();
+            ActiveAndroid.beginTransaction();
+            try {
+                for (int i = 0; i < clients.size(); i++) {
+                    Client client = clients.get(clients.size() - 1 - i);
+                    if (client.user != null) {
+                        client.cascadeSave();
+                    }
                 }
+                ActiveAndroid.setTransactionSuccessful();
+            } finally {
+                ActiveAndroid.endTransaction();
             }
         }
 
         // get inspections from server & save them locally
         List<Inspection> inspections = serverDB.getInspections();
         if (inspections != null && !inspections.isEmpty()) {
-            for (Inspection inspection : inspections) {
-                inspection.cascadeSave();
+            ActiveAndroid.beginTransaction();
+            try {
+                for (Inspection inspection : inspections) {
+                    inspection.cascadeSave();
+                }
+                ActiveAndroid.setTransactionSuccessful();
+            } finally {
+                ActiveAndroid.endTransaction();
             }
         }
 
         // get inspection images from server & save them locally
         List<InspectionImage> images = serverDB.getInspectionImages();
-        Http http = new Http();
         if (images != null && !images.isEmpty()) {
-            Type type = new TypeToken<List<InspectionImage>>() {
-            }.getType();
-            Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
-            Log.d("dbg", "@SignInIntentService: images is: " + gson.toJson(images, type));
-
-            for (InspectionImage image : images) {
-                // make sure image & corresponding thumbnail are actually in S3 bucket
-                if (validImage(http, image.path + ".jpg") && validImage(http, image.path + "_s.jpg")) {
+            ActiveAndroid.beginTransaction();
+            try {
+                for (InspectionImage image : images) {
                     image.save();
                 }
-                else{
-                    Log.d("dbg", "@SignInIntentService: image not saved due to invalid path: " + image.path);
-                }
+                ActiveAndroid.setTransactionSuccessful();
+            } finally {
+                ActiveAndroid.endTransaction();
             }
         }
 
         // remove any inspections which can't produce a thumbnail url
-        LocalDB localDB = new LocalDB();
         if (inspections != null && !inspections.isEmpty()) {
-            for (Iterator<Inspection> iterator = inspections.listIterator(); iterator.hasNext(); ) {
-                Inspection inspection = iterator.next();
-                String url = localDB.getInspectionThumbnail(inspection.id);
-                if (url == null) {
-                    iterator.remove();
-                    inspection.delete();
+            LocalDB localDB = new LocalDB();
+            ActiveAndroid.beginTransaction();
+            try {
+                for (Iterator<Inspection> iterator = inspections.listIterator(); iterator.hasNext(); ) {
+                    Inspection inspection = iterator.next();
+                    String url = localDB.getInspectionThumbnail(inspection.id);
+                    if (url == null) {
+                        iterator.remove();
+                        inspection.delete();
+                    }
                 }
+            } finally {
+                ActiveAndroid.setTransactionSuccessful();
             }
+            ActiveAndroid.endTransaction();
         }
-
 
         // update user sign in status
         new UserInfo().setUserStatus(this, true);
 
         // broadcast that service is complete
         sendBroadcast(new Intent().setAction(Params.SIGN_IN_SERVICE_COMPLETE));
-    }
-
-    private boolean validImage(Http http, String path) {
-        String url = Params.CLOUD_URL + path;
-        int code = 0;
-        try {
-            code = http.getRequestCode(url);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return (code == 200);
     }
 }
